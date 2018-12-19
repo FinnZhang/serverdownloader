59b0425152db1dbeb0b3978245f7092f946e325d
Finn Zhang <naturelover5@outlook.com>
2018-12-19 23:55:58 +0800

    add upload list and clear hander & add bootstrap to index.html

--- serverdownloader.go
+++ serverdownloader.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -51,9 +52,68 @@ func download(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	fileName := header.Filename
+	if _, err := os.Stat(".\\static"); os.IsNotExist(err) {
+		err := os.Mkdir(".\\static", 0777)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	newFile, err := os.Create("./static/" + fileName)
+	defer newFile.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.Copy(newFile, file)
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	html := `<ul>`
+	files, err := ioutil.ReadDir("static")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, file := range files {
+		html += `<li><a href="/static/` + file.Name() + `">` + file.Name() + `</a></li>`
+	}
+	html += "</ul>"
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err = w.Write([]byte(html))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func clear(w http.ResponseWriter, r *http.Request) {
+	if _, err := os.Stat(".\\static"); !os.IsNotExist(err) {
+		err := os.RemoveAll(".\\static")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", home)
 	http.HandleFunc("/dl", download)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/list", list)
+	http.HandleFunc("/clear", clear)
 	http.ListenAndServe(":80", nil)
 
 }
